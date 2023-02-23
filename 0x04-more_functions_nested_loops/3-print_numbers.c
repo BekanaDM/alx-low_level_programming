1f8ba778b23ad7f43a575a8770fb2f52ecc24213
BekanaDM <bekdmeta@gmail.com>
2023-02-23 03:47:16 -0800

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,12 @@
 /**
  * print_numbers - Prints the numbers, from 0 to 9.
  *
- * Return: 0 Always (Success)
  */
 void print_numbers(void)
 {
 char i;
 	for (i = '0'; i <= '9'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('/n');
+		_putchar(i + '0');
+
+	_putchar('\n');
 }
