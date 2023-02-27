54313ff0e102f86cc6863fea8d325e2cff9d8aa3
BekanaDM <bekdmeta@gmail.com>
2023-02-27 04:56:56 -0800

    m

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * swap_int - swaps the values of a and b
- *   * @a: pointer 1
- *    * @b: pointer 2
- *     * Return; 0
- *      */
+ * swap_int - swaps the values of a and b
+ * @s: pointer 1
+ * Return; 0
+ 
+ */
 
 int _strlen(char *s)
 {
