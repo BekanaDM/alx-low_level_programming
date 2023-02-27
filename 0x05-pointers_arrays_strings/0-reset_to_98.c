bdec2fc12e345f62a9567de76549fb854b862aa9
BekanaDM <bekdmeta@gmail.com>
2023-02-27 03:46:43 -0800

    m

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Checks if a character is uppercase.
+ * @n: The character to be checked.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	printf("Value of *n variable: %d\n", *n );
+}
+
