24b88890b12e52490d1b537a8f6c94999ccdef14
BekanaDM <bekdmeta@gmail.com>
2023-02-27 04:10:43 -0800

    m

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated.
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*temp = *a;
+	*b = *a;
+	*temp = *b;
+}
