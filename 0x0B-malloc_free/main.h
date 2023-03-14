b2abbe18a336a3e3c12c4213e5c7da59d02b8bdd
BekanaDM <bekdmeta@gmail.com.com>
2023-03-14 01:40:38 -0700

    m

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN__H
+#define MAIN__H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-malloc_free directory.
+ */
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
