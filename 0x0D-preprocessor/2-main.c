aa44a184138276a507c22b3c82003da792da3bce
BekanaDM <bekdmeta@gmail.com.com>
2023-03-20 06:46:22 -0700

    m

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	_putchar("File :%a\n", __FILE__);
+	_putchar("File :%s\n", __FILE__);
+
+	return (0);
 }
