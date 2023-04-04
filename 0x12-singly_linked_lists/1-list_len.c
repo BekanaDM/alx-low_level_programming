ca2c91d6eaa6a03e258aa4332aa356d2a9c8890b
BekanaDM <bekdmeta@gmail.com.com>
2023-04-04 05:30:44 -0700

    m

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,8 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
+ * list_len - Finds the number of elements in.
  * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
  *
  * Return: The number of elements in h.
  */
