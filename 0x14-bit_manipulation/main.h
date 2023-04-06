ba6a3c88d0180a5f02842ae0530e534e45a9245b
BekanaDM <bekdmeta@gmail.com.com>
2023-04-06 06:22:41 -0700

    bit_manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,11 +2,17 @@
 #define MAIN_H
 
 /*
-* Desc: header file containinf prototypes and definitions
-*       for all functions and types written in this proj
-*/
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
 
-#include <stdlib.h>
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-#endif
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* MAIN_H */
